8d48af3fc6ed3d59d6f8bdf99719a0fc7d572165
hoangthaiduong2001 <duongtantien1@gmail.com>
2025-03-11 21:31:20 +0700

    update code

---
+++ app/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderColor: "red",
+    borderWidth: 5,
+  },
+  welcomeText: {
+    flex: 0.6,
+    borderColor: "green",
+    borderWidth: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  welCome: {
+    flex: 0.4,
+    borderColor: "yellow",
+    borderWidth: 5,
+  },
+  heading: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  body: {
+    fontSize: 30,
+    color: "orange",
+    marginVertical: 10,
+  },
+  footer: {},
+});
+const WelcomePage = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.welcomeText}>
+        <Text style={styles.heading}>Welcome to</Text>
+        <Text style={styles.body}>Food app</Text>
+        <Text style={styles.footer}>Slogan</Text>
+      </View>
+      <View style={styles.welCome}>
+        <Text style={{ backgroundColor: "yellow" }}>Login with</Text>
+        <View>
+          <View style={styles.welCome}>
+            <View style={styles.welCome}>
+              <Text>Facebook</Text>
+            </View>
+            <View style={styles.welCome}>
+              <Text>Google</Text>
+            </View>
+          </View>
+          <View style={styles.welCome}>
+            <Text>Login with email</Text>
+          </View>
+          <View style={styles.welCome}>
+            <Text>You don't have account? Register</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default WelcomePage;
